6ea5237143bd145888d114b83adcc911946a2799
Michaelk <gw.rexxar@gmail.com>
2016-05-26 16:46:04 +0300

    added untracked file

---
+++ DoGoService/Paths/Enums/NodeAction.cs
@@ -0,0 +1,17 @@
+﻿using Google.Maps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoGoService.Paths.Enums
+{
+    public enum NodeAction
+    {
+        WalkPickup = 1,
+        WalkReturn,
+        Walk,
+        Wait,
+        Start
+    }
+}
